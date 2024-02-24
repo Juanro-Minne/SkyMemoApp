5cf5971b4da008221237841c8e61441695f06411
Juanro-Minne <jminne@retrorabbit.co.za>
2024-02-24 13:24:21 +0200

    added toggle  and tabview

--- flightlogbook_app/flight_logbook/lib/components/custom_tabbar.dart
+++ flightlogbook_app/flight_logbook/lib/components/custom_tabbar.dart
@@ -2,29 +2,20 @@ import 'package:flutter/material.dart';
 
 class CustomTabBar extends StatelessWidget {
   final TabController? tabController;
+  final List<Widget> children;
 
-  const CustomTabBar({Key? key, this.tabController}) : super(key: key);
+  const CustomTabBar({
+    Key? key,
+    this.tabController,
+    required this.children,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TabBar(
       controller: tabController,
-      indicator: BoxDecoration(
-        color: Colors.green.shade900, // Dark green color
-        borderRadius: BorderRadius.circular(50),
-      ),
-      labelColor: const Color.fromARGB(255, 212, 198, 106), // Gold color
-      unselectedLabelColor: Colors.white, // White color
-      tabs: const [
-        Tab(
-          text: 'Add Flight',
-        ),
-        Tab(
-          text: 'View Flights',
-        ),
-      ],
+      unselectedLabelColor: Colors.black,
+      tabs: children,
     );
   }
 }
-
-
