1a75140da3017807c2e58fa8e44a814542b973ba
Juanro-Minne <jminne@retrorabbit.co.za>
2024-03-31 21:15:04 +0200

    fixed warning

--- flightlogbook_app/flight_logbook/lib/screens/documents_screen.dart
+++ flightlogbook_app/flight_logbook/lib/screens/documents_screen.dart
@@ -1,5 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, deprecated_member_use
-
+// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, deprecated_member_use, avoid_function_literals_in_foreach_calls
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
